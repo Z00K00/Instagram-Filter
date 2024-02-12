6ba7ce07d9424e7e41353fa28eb5207eb006df31
kraiglarson2 <kraiglarson2@gmail.com>
2024-02-12 15:34:08 -0500

    Initial Commit

---
+++ InstaFilter/InstaFilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstaFilterApp.swift
+//  InstaFilter
+//
+//  Created by Krajg Larson on 2/12/24.
+//
+
+import SwiftUI
+
+@main
+struct InstaFilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
